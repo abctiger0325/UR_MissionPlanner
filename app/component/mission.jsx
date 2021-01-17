0b15422b40220303f7693ee474d926cc6548d68f
abctiger0325 <abctiger0325@gmail.com>
2021-01-17 22:41:37 +0800

    20210117 React Position

--- app/component/mission.jsx
+++ app/component/mission.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import XMLData from '../src/mission.xml';
+import RGL, { WidthProvider, GridLayout  } from "react-grid-layout";
 
 // var rawFile = new XMLHttpRequest();
 // rawFile.open("GET", XMLData, false);
@@ -17,21 +18,122 @@ export class Mission extends React.Component {
         // this.state = {
         //     data: ''
         // }
+        let missions = XMLData.Plan.Mission.map((d, i) => {
+            console.log(d)
+            let name = d.$.prereg === undefined ? "MissionContainer" : "MissionContainer Disabled";
+            return (
+                <div
+                    key={i}
+                    className={name}
+                    {...d.$}
+                >
+                    {d._}
+                </div>)
+        })
+
+        let layout = XMLData.Plan.Mission.map((d, i) => {
+            // console.log(d)
+            return {
+                x: 0,
+                y: i,
+                w: 3,
+                h: 1,
+                i: d.$.id,
+                // isDraggable: able
+            }
+        })
+        this.mission = React.createRef();
+        this.state = {
+            missions,layout,
+            draggable: this.props.draggable,
+            limit: []
+        }
+        console.log(this.state)
     }
 
-    // componentDidMount(){
+    componentDidUpdate(prevProps){
+        if (prevProps.draggable !== this.props.draggable){
+            let limit = [];
+            let layout = XMLData.Plan.Mission.map((d, i) => {
+                // console.log(d)
+                if (d.$.prereg !== undefined) {
+                    let obj = XMLData.Plan.Mission.find(e => e.$.id === d.$.prereg);
+                    // console.log(obj);
+                    limit.push({
+                        preId : obj.$.id,
+                        limitPlace: i
+                    })
+                }
+                return {
+                    x: 0,
+                    y: i,
+                    w: 3,
+                    h: 1,
+                    i: i.toString(),
+                    // isDraggable: able
+                }
+            })
 
-    // }
+            this.setState({
+                layout,
+                draggable: this.props.draggable,
+                limit
+            },() => console.log(this.state.limit))
+        }
+    }
+
+    handleDrag(lay, oldItem, newItem, placeholder, e, element){
+        // console.log(element.getAttribute("id"));
+        const {limit,layout,missions} = this.state;
+        let filterRes = limit.filter(x => x.preId === element.getAttribute("id"))
+        console.log(filterRes)
+        if ((filterRes.length > 0) && (newItem.y >= filterRes[0].limitPlace)){
+            this.setState({layout})
+            return
+        }
+        // console.log(missions,oldItem)
+        const obj = missions.splice(oldItem.y,1);
+        missions.splice(newItem.y,0,obj[0]);
+        let buf = missions.map((d,i) => {
+            console.log(d.key , i.toString());
+            if (d.key === i.toString()) return d;
+            let data = XMLData.Plan.Mission[d.key]
+            let name = data.$.prereg === undefined ? "MissionContainer" : "MissionContainer Disabled";
+            console.log(data)
+            return (
+                <div
+                    key={i}
+                    className={name}
+                    {...data.$}
+                >
+                    {data._}
+                </div>
+            )
+
+        })
+        console.log(buf)
+        this.setState({missions:buf})
+    }
 
     render() {
-        console.log(XMLData);
-        let missions = XMLData.Plan.Mission.map((d,i) => {
-            console.log(d)
-            return (<div key={i}>{d._}</div>)
-        })
+        const {missions, layout, draggable} = this.state
+        console.log(draggable);
+        const ReactGridLayout = WidthProvider(RGL);
+
         return (
             <>
-                {missions}
+                <ReactGridLayout
+                    layout={layout}
+                    isResizable={false}
+                    isDraggable={draggable}
+                    cols = {1}
+                    rowHeight = {60}
+                    width = {600}
+                    draggableCancel=".Disabled"
+                    onDragStop={(layout, oldItem, newItem, placeholder, e, element) => this.handleDrag(layout, oldItem, newItem, placeholder, e, element)}
+                >
+                    {missions}
+                </ReactGridLayout>
             </>
         )
     }
--- app/mainWindow.jsx
+++ app/mainWindow.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Switch from "react-switch";
+
 import { Counter } from './component/counter.jsx';
-import { Mission } from './component/mission.jsx';
+import { Mission }  from './component/mission.jsx';
+// import { Planner } from './component/planner.jsx';
+
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            // second: '',
+            planning: false,
             // minute: '',
 
         }
         // this.handleTextChange = this.handleTextChange.bind(this);
     }
 
-    // handleTextChange(e) {
-    // this.setState({ message: e.target.value });
-    // }
 
 
     render(){
@@ -25,7 +26,29 @@ class App extends React.Component {
                 Time Counter: 
                 <Counter/>
                 Mission: 
-                <Mission/>
+                <Switch
+                    checked={this.state.planning}
+                    onChange={() => {
+                        const {planning} = this.state;
+                        this.setState({
+                            planning: !planning
+                        })
+                    }}
+                    onColor="#86d3ff"
+                    onHandleColor="#2693e6"
+                    handleDiameter={20}
+                    // uncheckedIcon={false}
+                    // checkedIcon={false}
+                    boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                    activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                    height={20}
+                    width={48}
+                    className="react-switch"
+                    id="material-switch"
+                />
+                <Mission
+                    draggable={this.state.planning}
+                />
             </div>
         )
     }
