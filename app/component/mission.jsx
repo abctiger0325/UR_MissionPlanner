3784aa585928a3af8eb66c5c428cf52ac429ae57
abctiger0325 <abctiger0325@gmail.com>
2021-01-19 23:25:48 +0800

    20210119

--- app/component/mission.jsx
+++ app/component/mission.jsx
@@ -18,6 +18,61 @@ export class Mission extends React.Component {
         // this.state = {
         //     data: ''
         // }
+        // let missions = XMLData.Plan.Mission.map((d, i) => {
+        //     console.log(d)
+        //     let name = d.$.prereg === undefined ? "MissionContainer" : "MissionContainer Disabled";
+        //     return (
+        //         <div
+        //             key={i}
+        //             className={name}
+        //             {...d.$}
+        //         >
+        //             {d._}
+        //         </div>)
+        // })
+
+        let missions = this.renderDOM();
+
+        let res = this.renderLayout();
+        let layout = res[0];
+        let limit = res[1];
+
+        this.state = {
+            missions,layout,
+            draggable: this.props.draggable,
+            limit
+        }
+        console.log(this.state)
+    }
+
+    componentDidUpdate(prevProps){
+        if (prevProps.draggable !== this.props.draggable){
+            let res = this.renderLayout();
+            let layout = res[0];
+            let limit = res[1];
+
+            this.setState({
+                layout,
+                draggable: this.props.draggable,
+                limit
+            },() => console.log(this.state.limit))
+        }
+    }
+
+    inRange(e,rule,newPos){
+        let obj = e.getAttribute("id");
+        let preRule = rule.filter(x => (x.preId) === obj);
+        let reqRule = rule.filter(x => (x.regId) === obj);
+        let prePass = true, reqPass = true;
+        // console.log(preRule, reqRule,newPos)
+
+        if (preRule.length > 0) prePass = (newPos < preRule[0].limitPlaceLower);
+        if (reqRule.length > 0) reqPass = (newPos > reqRule[0].limitPlaceUpper);
+        // console.log(prePass,reqPass)
+        return prePass && reqPass;
+    }
+
+    renderDOM(){
         let missions = XMLData.Plan.Mission.map((d, i) => {
             console.log(d)
             let name = d.$.prereg === undefined ? "MissionContainer" : "MissionContainer Disabled";
@@ -30,89 +85,63 @@ export class Mission extends React.Component {
                     {d._}
                 </div>)
         })
+        return missions
+    }
 
+    renderLayout(){
+        let limit = [];
         let layout = XMLData.Plan.Mission.map((d, i) => {
             // console.log(d)
+            if (d.$.prereg !== undefined) {
+                let obj = XMLData.Plan.Mission.find(e => e.$.id === d.$.prereg);
+                let num = XMLData.Plan.Mission.findIndex(e => e.$.id === d.$.prereg);
+                // console.log(num)
+                limit.push({
+                    preId: obj.$.id,
+                    limitPlaceUpper: num,
+                    limitPlaceLower: i,
+                    regId: d.$.id
+                })
+            }
             return {
                 x: 0,
                 y: i,
                 w: 3,
                 h: 1,
-                i: d.$.id,
+                i: i.toString(),
                 // isDraggable: able
             }
         })
-        this.mission = React.createRef();
-        this.state = {
-            missions,layout,
-            draggable: this.props.draggable,
-            limit: []
-        }
-        console.log(this.state)
-    }
-
-    componentDidUpdate(prevProps){
-        if (prevProps.draggable !== this.props.draggable){
-            let limit = [];
-            let layout = XMLData.Plan.Mission.map((d, i) => {
-                // console.log(d)
-                if (d.$.prereg !== undefined) {
-                    let obj = XMLData.Plan.Mission.find(e => e.$.id === d.$.prereg);
-                    // console.log(obj);
-                    limit.push({
-                        preId : obj.$.id,
-                        limitPlace: i
-                    })
-                }
-                return {
-                    x: 0,
-                    y: i,
-                    w: 3,
-                    h: 1,
-                    i: i.toString(),
-                    // isDraggable: able
-                }
-            })
-
-            this.setState({
-                layout,
-                draggable: this.props.draggable,
-                limit
-            },() => console.log(this.state.limit))
-        }
+        return [layout,limit]
     }
 
     handleDrag(lay, oldItem, newItem, placeholder, e, element){
-        // console.log(element.getAttribute("id"));
+        // console.log(element.getAttribute("id"),newItem.y);
         const {limit,layout,missions} = this.state;
-        let filterRes = limit.filter(x => x.preId === element.getAttribute("id"))
-        console.log(filterRes)
-        if ((filterRes.length > 0) && (newItem.y >= filterRes[0].limitPlace)){
-            this.setState({layout})
-            return
-        }
-        // console.log(missions,oldItem)
-        const obj = missions.splice(oldItem.y,1);
-        missions.splice(newItem.y,0,obj[0]);
-        let buf = missions.map((d,i) => {
-            console.log(d.key , i.toString());
-            if (d.key === i.toString()) return d;
-            let data = XMLData.Plan.Mission[d.key]
-            let name = data.$.prereg === undefined ? "MissionContainer" : "MissionContainer Disabled";
-            console.log(data)
-            return (
-                <div
-                    key={i}
-                    className={name}
-                    {...data.$}
-                >
-                    {data._}
-                </div>
-            )
 
+        let filterRes = limit.filter(x => (x.preId === element.getAttribute("id") || x.regId === element.getAttribute("id")))
+        // console.log(filterRes)
+        if (filterRes.length > 0){
+            if (!this.inRange(element,filterRes,newItem.y)){
+                this.setState({layout})
+                return
+            }
+        }
+        const list = XMLData.Plan.Mission;
+        const listObj = list.splice(oldItem.y, 1);
+        list.splice(newItem.y, 0, listObj[0])
+        let missionsNew = this.renderDOM();
+
+        let res = this.renderLayout();
+        let layoutNew = res[0];
+        let limitNew = res[1];
+
+        console.log(list,XMLData.Plan.Mission)
+        this.setState({
+            missions:missionsNew, 
+            layout:layoutNew,
+            limit:limitNew
         })
-        console.log(buf)
-        this.setState({missions:buf})
     }
 
     render() {
@@ -129,7 +158,7 @@ export class Mission extends React.Component {
                     cols = {1}
                     rowHeight = {60}
                     width = {600}
-                    draggableCancel=".Disabled"
+                    // draggableCancel=".Disabled"
                     onDragStop={(layout, oldItem, newItem, placeholder, e, element) => this.handleDrag(layout, oldItem, newItem, placeholder, e, element)}
                 >
                     {missions}
